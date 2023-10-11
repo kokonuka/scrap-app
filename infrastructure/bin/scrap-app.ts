c10e9bfe84d77cf7766758b3415002183310fbd6
sun-njp <miuraasahi1@gmail.com>
2023-10-11 19:50:16 +0900

    プロジェクト名に関する修正

--- infrastructure/bin/scrap-app.ts
+++ infrastructure/bin/scrap-app.ts
@@ -3,4 +3,4 @@ import * as cdk from "aws-cdk-lib";
 import { ScrapAppStack } from "../lib/scrap-app-stack";
 
 const app = new cdk.App();
-new ScrapAppStack(app, "InfrastructureStack");
+new ScrapAppStack(app, "ScrapAppStack");
