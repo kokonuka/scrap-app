77b7b6005f7ca286f15efbe6d9366149a6b43ce9
sun-njp <miuraasahi1@gmail.com>
2023-10-12 00:22:04 +0900

    api gatewayとlambdaを構築

--- infrastructure/lib/scrap-app-stack.ts
+++ infrastructure/lib/scrap-app-stack.ts
@@ -1,19 +1,29 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import * as sns from "aws-cdk-lib/aws-sns";
-import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
-import * as sqs from "aws-cdk-lib/aws-sqs";
+import { Stack, StackProps, aws_apigateway, aws_lambda } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class ScrapAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, "ScrapAppQueue", {
-      visibilityTimeout: Duration.seconds(300),
+    const fn = new aws_lambda.Function(this, "Lambda", {
+      functionName: "scrap-app",
+      code: aws_lambda.Code.fromAsset("../backend/dist"),
+      handler: "index.handler",
+      runtime: aws_lambda.Runtime.NODEJS_18_X,
+      environment: {
+        NODE_OPTIONS: "--enable-source-maps",
+      },
     });
 
-    const topic = new sns.Topic(this, "ScrapAppTopic");
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const api = new aws_apigateway.RestApi(this, "Api", {
+      defaultCorsPreflightOptions: {
+        allowOrigins: aws_apigateway.Cors.ALL_ORIGINS,
+        allowMethods: aws_apigateway.Cors.ALL_METHODS,
+        allowHeaders: aws_apigateway.Cors.DEFAULT_HEADERS,
+      },
+    });
+    api.root.addProxy({
+      defaultIntegration: new aws_apigateway.LambdaIntegration(fn),
+    });
   }
 }
