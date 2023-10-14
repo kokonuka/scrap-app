90697403fc83b3604162ef8767cd54aff44aff5a
sun-njp <miuraasahi1@gmail.com>
2023-10-14 19:42:51 +0900

    ExpressのCORS対応

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
 import serverlessExpress from "@vendia/serverless-express";
+import cors from "cors";
 import { scrapRouter } from "./presentation/scrapRouter";
 import { scrapItemRouter } from "./presentation/scrapItemRouter";
 
 const PORT = 3001;
 
 const app: Express = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use("/scraps", scrapRouter);
