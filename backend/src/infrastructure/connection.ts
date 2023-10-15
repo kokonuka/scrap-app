c9f9c9881fe587f73b54d72fa0cf2e500e700bde
sun-njp <miuraasahi1@gmail.com>
2023-10-16 02:12:33 +0900

    DBの接続先を環境で制御

--- backend/src/infrastructure/connection.ts
+++ backend/src/infrastructure/connection.ts
@@ -1,10 +1,16 @@
 import { DynamoDBClient, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const config: DynamoDBClientConfig = {
-  endpoint: "http://localhost:8000",
-  // region: "ap-northeast-1",
-  // credentials: { accessKeyId: "FAKE", secretAccessKey: "FAKE" },
-};
+require("dotenv").config();
+
+const config: DynamoDBClientConfig =
+  process.env.NODE_ENV === "development"
+    ? {
+        endpoint: "http://localhost:8000",
+      }
+    : {
+        region: "ap-northeast-1",
+      };
+
 const dbClient = new DynamoDBClient(config);
 export const documentClient = DynamoDBDocumentClient.from(dbClient);
