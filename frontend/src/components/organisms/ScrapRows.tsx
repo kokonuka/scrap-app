4e9ee8ff795f95bbe3c8d4f80e401e9fd6623dbb
sun-njp <miuraasahi1@gmail.com>
2023-10-17 21:27:13 +0900

    エラーを解消

--- frontend/src/components/organisms/ScrapRows.tsx
+++ frontend/src/components/organisms/ScrapRows.tsx
@@ -21,7 +21,6 @@ export const ScrapRows: React.FC = () => {
                 <ScrapRow
                   setScraps={setScraps}
                   scrapId={scrap.id}
-                  isLoading={isLoading}
                   key={scrap.id}
                 />
               ))}
