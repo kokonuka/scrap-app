f7daf07984f806116b19c91790426832fa955b75
sun-njp <miuraasahi1@gmail.com>
2023-10-17 12:22:32 +0900

    レイアウトを修正

--- frontend/src/components/molecules/ScrapThreadItem.tsx
+++ frontend/src/components/molecules/ScrapThreadItem.tsx
@@ -151,8 +151,8 @@ export const ScrapThreadItem: React.FC<Props> = ({
         <Box mt="0.8rem">
           {!isEdit ? (
             <Box display="flex">
-                {content}
               <Box flex="1" whiteSpace="pre-line" lineHeight="1.7">
+                {content}
               </Box>
             </Box>
           ) : (
