71d8e33307586c00cdd7ec0d3a47c7582c2bbfdc
sun-njp <miuraasahi1@gmail.com>
2023-10-18 00:39:20 +0900

    メタ情報を追加

--- frontend/src/components/Head.tsx
+++ frontend/src/components/Head.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import NextHead from "next/head";
 
 export const Head: React.FC = () => {
+  const title = "SCRAPS | スクラップ記録サービス";
+  const description =
+    "SCRAPSはスクラップ記録サービスです。スレッド形式でメモを時系列に整理していくことができます。何か新しいことをキャッチアップする際、「情報量が多くまとまらない」「後でまとめようと思っていたけど思い出せない」なんてことはありませんか？SCRAPSではリアルタイムで考えていることや参考資料といった事柄をメモしていくことでそのような問題を解決できます";
+
   return (
     <NextHead>
-      <title>SCRAPS | スクラップ記録サービス</title>
-      <meta
-        name="description"
-        content="SCRAPSはスクラップ記録サービスです。スレッド形式でメモを時系列に整理していくことができます。何か新しいことをキャッチアップする際、「情報量が多くまとまらない」「後でまとめようと思っていたけど思い出せない」なんてことはありませんか？SCRAPSではリアルタイムで考えていることや参考資料といった事柄をメモしていくことでそのような問題を解決できます"
-      />
+      <title>{title}</title>
+      <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <meta property="og:title" content={title} />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="/boxes.png" />
+      <link rel="manifest" href="/manifest.webmanifest"></link>
     </NextHead>
   );
 };
