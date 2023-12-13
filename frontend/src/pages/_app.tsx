d03ad521eed88f3e1b49b13123df9259c9156153
sun-njp <miuraasahi1@gmail.com>
2023-12-13 10:32:59 +0900

    フォントを変更

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Noto_Sans_JP } from "next/font/google";
+
+const noto = Noto_Sans_JP({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-noto-sans-jp",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <div className={`${noto.className}`}>
+        <Component {...pageProps} />
+      </div>
     </ChakraProvider>
   );
 }
