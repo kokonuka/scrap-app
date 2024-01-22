1d58c811f10f923a9731871bb6dc8df3a774fbc1
sun-njp <miuraasahi1@gmail.com>
2024-01-22 23:39:59 +0900

    スクラップ名が変わらない問題を修正

--- backend/src/infrastructure/scrapGateway.ts
+++ backend/src/infrastructure/scrapGateway.ts
@@ -101,12 +101,14 @@ export class ScrapGateway {
 
   async create(title: string) {
     const uniqueId = uuidv4();
+    const isOpen = true;
 
     const params: PutCommandInput = {
       TableName: TABLE_NAME,
       Item: {
         id: uniqueId,
-        title: title,
+        title,
+        isOpen,
         createdAt: new Date().toISOString(),
         updatedAt: new Date().toISOString(),
         scrap: "scrap",
@@ -122,6 +124,7 @@ export class ScrapGateway {
   }
 
   async update(item: Record<string, any>) {
+    console.log("item", item);
     const params: UpdateCommandInput = {
       TableName: TABLE_NAME,
       Key: { id: item.id },
